01c82a1f2e49078b3eb91b5f9e577973bda1cf79
Alexander Brandes <mc.cache@web.de>
2025-04-22 13:21:00 +0200

    Move to new central portal
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,18 @@
 import com.diffplug.gradle.spotless.SpotlessPlugin
+import com.vanniktech.maven.publish.SonatypeHost
 import java.net.URI
 
 plugins {
     java
-    `maven-publish`
     signing
 
     id("com.diffplug.spotless") version "7.0.3"
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.31.0"
 }
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://maven.enginehub.org/repo/")
-    }
+    maven("https://maven.enginehub.org/repo/")
 }
 
 dependencies {
@@ -48,30 +46,10 @@ spotless {
 }
 
 tasks {
-    compileJava {
-        options.compilerArgs.addAll(arrayOf("-Xmaxerrs", "1000"))
-        options.compilerArgs.add("-Xlint:all")
-        for (disabledLint in arrayOf("processing", "path", "fallthrough", "serial"))
-            options.compilerArgs.add("-Xlint:$disabledLint")
-        options.isDeprecation = true
-        options.encoding = "UTF-8"
-    }
 
     javadoc {
-        title = project.name + " " + project.version
         val opt = options as StandardJavadocDocletOptions
-        opt.addStringOption("Xdoclint:none", "-quiet")
-        opt.tags(
-            "apiNote:a:API Note:",
-            "implSpec:a:Implementation Requirements:",
-            "implNote:a:Implementation Note:"
-        )
-        opt.addBooleanOption("html5", true)
-        opt.links("https://javadoc.io/doc/org.jetbrains/annotations/24.1.0/")
-        opt.isLinkSource = true
-        opt.isUse = true
-        opt.encoding("UTF-8")
-        opt.keyWords()
+        opt.links("https://javadoc.io/doc/org.jetbrains/annotations/26.0.2/")
         opt.noTimestamp()
     }
 
@@ -91,68 +69,55 @@ signing {
     }
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
+mavenPublishing {
+    coordinates(
+        groupId = "$group",
+        artifactId = project.name,
+        version = "${project.version}",
+    )
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
+    pom {
+        name.set(project.name)
+        description.set("Java client for the Arkitektonika API")
+        url.set("https://github.com/IntellectualSites/Arkitektonika-Client")
 
-            pom {
-
-                name.set(project.name + " " + project.version)
-                description.set("Java client for the Arkitektonika API")
-                url.set("https://github.com/IntellectualSites/Arkitektonika-Client")
-
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/licenses/MIT")
-                        distribution.set("repo")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("Citymonstret")
-                        name.set("Alexander Söderberg")
-                        organization.set("IntellectualSites")
-                        organizationUrl.set("https://github.com/IntellectualSites")
-                    }
-                    developer {
-                        id.set("NotMyFault")
-                        name.set("Alexander Brandes")
-                        organization.set("IntellectualSites")
-                        organizationUrl.set("https://github.com/IntellectualSites")
-                        email.set("contact(at)notmyfault.dev")
-
-                    }
-                }
-
-                scm {
-                    url.set("https://github.com/IntellectualSites/Arkitektonika-Client")
-                    connection.set("scm:git:https://github.com/IntellectualSites/Arkitektonika-Client.git")
-                    developerConnection.set("scm:git:git@github.com:IntellectualSites/Arkitektonika-Client.git")
-                    tag.set("${project.version}")
-                }
-
-                issueManagement{
-                    system.set("GitHub")
-                    url.set("https://github.com/IntellectualSites/Arkitektonika-Client/issues")
-                }
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("repo")
             }
         }
-    }
-}
 
-nexusPublishing {
-    this.repositories {
-        sonatype {
-            nexusUrl.set(URI.create("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(URI.create("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        developers {
+            developer {
+                id.set("Citymonstret")
+                name.set("Alexander Söderberg")
+                organization.set("IntellectualSites")
+                organizationUrl.set("https://github.com/IntellectualSites")
+            }
+            developer {
+                id.set("NotMyFault")
+                name.set("Alexander Brandes")
+                organization.set("IntellectualSites")
+                organizationUrl.set("https://github.com/IntellectualSites")
+                email.set("contact(at)notmyfault.dev")
+
+            }
         }
+
+        scm {
+            url.set("https://github.com/IntellectualSites/Arkitektonika-Client")
+            connection.set("scm:git:https://github.com/IntellectualSites/Arkitektonika-Client.git")
+            developerConnection.set("scm:git:git@github.com:IntellectualSites/Arkitektonika-Client.git")
+            tag.set("${project.version}")
+        }
+
+        issueManagement{
+            system.set("GitHub")
+            url.set("https://github.com/IntellectualSites/Arkitektonika-Client/issues")
+        }
+
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     }
 }
